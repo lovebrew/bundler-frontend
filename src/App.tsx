d91a411488d8610b29226bc47ad3430e4d80b4ab
TurtleP <jpostelnek@outlook.com>
2023-07-14 12:52:19 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
 import MainLogo from "./Components/MainLogo"
 import Footer from "./Components/Footer"
-import CustomModal from "./Components/CustomModal"
+import AlertDialog from "./Components/CustomModal"
 
 import { createTheme, ThemeProvider } from "@mui/material";
-import BasicModal from './Components/CustomModal';
 
 const theme = createTheme({
     palette: {
@@ -18,7 +17,7 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
             <MainLogo />
-            <BasicModal />
+            <AlertDialog />
             <Footer />
         </ThemeProvider>
     )
