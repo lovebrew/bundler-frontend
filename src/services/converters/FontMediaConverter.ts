7427e8a8306044c297489814e43b443131dd9fb1
TurtleP <jpostelnek@outlook.com>
2023-11-01 16:56:16 -0400

    make sure to validate fonts

--- src/services/converters/FontMediaConverter.ts
+++ src/services/converters/FontMediaConverter.ts
@@ -1,11 +1,21 @@
 import MediaConverter, { MediaFile } from "./MediaConverter";
 
 export default class FontMediaConverter extends MediaConverter {
+  async checkFont(file: MediaFile) {
+    const font = new FontFace("test", await file.data.arrayBuffer());
+    await font.load();
+  }
+
   async convert(files: MediaFile[]): Promise<MediaFile[]> {
     const body = new FormData();
 
     for (const file of files) {
-      body.append(file.filepath, file.data);
+      try {
+        await this.checkFont(file);
+        body.append(file.filepath, file.data);
+      } catch (error) {
+        throw new Error(`Font ${file.filepath} is invalid!`);
+      }
     }
 
     const response = await this.sendRequest("POST", body);
